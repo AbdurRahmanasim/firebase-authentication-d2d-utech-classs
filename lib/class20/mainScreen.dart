d7dcf28af0f0cc12511f426221a29a5ccb7781ab
AbdurRahmanasim <abdurrahmanasim7@gmail.com>
2025-02-08 21:24:13 +0500

    working of sidebar conditions

--- lib/class20/mainScreen.dart
+++ lib/class20/mainScreen.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:authtesting/class20/screens/analytics.dart';
 import 'package:authtesting/class20/screens/dashboard.dart';
 import 'package:authtesting/class20/screens/home.dart';
 import 'package:authtesting/class20/screens/profile.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sidebarx/sidebarx.dart';
 
 class MainScreen extends StatefulWidget {
@@ -21,9 +24,38 @@ class _MainScreenState extends State<MainScreen> {
     Profile()
   ];
 
+  final List<IconData> icons = const [
+    Icons.home,
+    Icons.dashboard,
+    Icons.analytics,
+    Icons.person,
+  ];
+
+  final List<String> screenNames = const [
+    "Home",
+    "Dashboard",
+    "Analytics",
+    "Profile"
+  ];
+
+  final Map permissions = {
+    "User" : [0,1,3],
+    "Shop Owner" : [0,1],
+    "Admin" :   [0,1,2,3],
+  };
+
   int activeScreen = 0;
   // final scaffoldKey = GlobalKey<ScaffoldState>();
   final _controller = SidebarXController(selectedIndex: 0, extended: true);
+
+  Future getuserData() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+    var getData = json.decode(_prefs.getString("userDetail") ?? "");
+    print(getData.runtimeType);
+
+    return getData;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,100 +66,107 @@ class _MainScreenState extends State<MainScreen> {
         backgroundColor: Colors.lightGreenAccent,
         title: Text("MainScreen"),
       ),
-      drawer: SidebarX(
-        controller: _controller,
-        theme: SidebarXTheme(
-          margin: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: canvasColor,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          hoverColor: scaffoldBackgroundColor,
-          textStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
-          selectedTextStyle: const TextStyle(color: Colors.white),
-          itemTextPadding: const EdgeInsets.only(left: 30),
-          selectedItemTextPadding: const EdgeInsets.only(left: 30),
-          itemDecoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: canvasColor),
-          ),
-          selectedItemDecoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: actionColor.withOpacity(0.37),
-            ),
-            gradient: const LinearGradient(
-              colors: [accentCanvasColor, canvasColor],
-            ),
-            boxShadow: [
-              const BoxShadow(
-                color: Color.fromARGB(255, 120, 62, 124),
-                blurRadius: 30,
-              )
-            ],
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.white.withOpacity(0.7),
-            size: 20,
-          ),
-          selectedIconTheme: const IconThemeData(
-            color: Colors.white,
-            size: 20,
-          ),
-        ),
-        extendedTheme: const SidebarXTheme(
-          width: 200,
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 53, 1, 51),
-          ),
-        ),
-        footerDivider: divider,
-        headerBuilder: (context, extended) {
-          return SizedBox(
-            height: 100,
-            child: Padding(
-              padding: const EdgeInsets.all(22.0),
-              child: Image.asset(
-                "assets/logo5.png",
+      drawer: FutureBuilder(
+        future: getuserData(),
+        builder: (context, snapshot) {
+          return SidebarX(
+            controller: _controller,
+            theme: SidebarXTheme(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: canvasColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              hoverColor: scaffoldBackgroundColor,
+              textStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
+              selectedTextStyle: const TextStyle(color: Colors.white),
+              itemTextPadding: const EdgeInsets.only(left: 30),
+              selectedItemTextPadding: const EdgeInsets.only(left: 30),
+              itemDecoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: canvasColor),
+              ),
+              selectedItemDecoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: actionColor.withOpacity(0.37),
+                ),
+                gradient: const LinearGradient(
+                  colors: [accentCanvasColor, canvasColor],
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 120, 62, 124),
+                    blurRadius: 30,
+                  )
+                ],
+              ),
+              iconTheme: IconThemeData(
+                color: Colors.white.withOpacity(0.7),
+                size: 20,
+              ),
+              selectedIconTheme: const IconThemeData(
                 color: Colors.white,
+                size: 20,
               ),
             ),
+            extendedTheme: const SidebarXTheme(
+              width: 200,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 53, 1, 51),
+              ),
+            ),
+            footerDivider: divider,
+            headerBuilder: (context, extended) {
+              return SizedBox(
+                height: 100,
+                child: Padding(
+                  padding: const EdgeInsets.all(22.0),
+                  child: Image.asset(
+                    "assets/logo5.png",
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            items: [
+              for (int i = 0; i < screens.length; i++)
+                if (snapshot.data['role'] ==)
+                  SidebarXItem(
+                    icon: icons[i],
+                    label: screenNames[i],
+                    onTap: () {
+                      activeScreen = 0;
+                      setState(() {});
+                    },
+                  )
+              // SidebarXItem(
+              //   icon: Icons.dashboard,
+              //   label: 'Dashboard',
+              //   onTap: () {
+              //     activeScreen = 1;
+              //     setState(() {});
+              //   },
+              // ),
+              // SidebarXItem(
+              //   icon: Icons.dashboard,
+              //   label: 'Analytics',
+              //   onTap: () {
+              //     activeScreen = 2;
+              //     setState(() {});
+              //   },
+              // ),
+              // SidebarXItem(
+              //   icon: Icons.person,
+              //   label: 'Profile',
+              //   onTap: () {
+              //     activeScreen = 3;
+              //     setState(() {});
+              //   },
+              // ),
+            ],
           );
         },
-        items: [
-          SidebarXItem(
-            icon: Icons.home,
-            label: 'Home',
-            onTap: () {
-              activeScreen = 0;
-              setState(() {});
-            },
-          ),
-          SidebarXItem(
-            icon: Icons.dashboard,
-            label: 'Dashboard',
-            onTap: () {
-              activeScreen = 1;
-              setState(() {});
-            },
-          ),
-          SidebarXItem(
-            icon: Icons.dashboard,
-            label: 'Analytics',
-            onTap: () {
-              activeScreen = 2;
-              setState(() {});
-            },
-          ),
-          SidebarXItem(
-            icon: Icons.person,
-            label: 'Profile',
-            onTap: () {
-              activeScreen = 3;
-              setState(() {});
-            },
-          ),
-        ],
       ),
       body: SafeArea(
         child: screens[activeScreen],
